0981d467f1ab1773e59ce88be22d66300cd04388
Adrian <adrianscibisz@gmail.com>
2021-03-15 13:08:38 +0100

    Min 2 done

--- Lab1 Webowe/src/index.js
+++ Lab1 Webowe/src/index.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log("test");
+class MinApp {
+    constructor() {
+        this.arrs = [];
+        this.inputs = document.querySelectorAll(".inputs > input");
+        this.outputs = document.querySelectorAll(".outputs > input");
+        this.addEvents(this.inputs);
+    }
+    addEvents(input) {
+        input.forEach((e) => {
+            e.addEventListener("input", () => {
+                // this is a value from single input 
+                // do 2 things with that p
+                this.arrs.push(+e.value);
+                this.display();
+            });
+        });
+    }
+    sum() {
+        return this.arrs.reduce((a, b) => a + b);
+    }
+    avg() {
+        return this.sum() / this.arrs.length;
+    }
+    sort() {
+        return this.arrs.sort((a, b) => a - b);
+    }
+    display() {
+        this.outputs.item(0).value = this.sum().toString();
+        this.outputs.item(1).value = this.avg().toString();
+        this.outputs.item(2).value = this.sort()[0].toString();
+        this.outputs.item(3).value = this.sort()[this.arrs.length - 1].toString();
+    }
+}
+const test = new MinApp();
--- Lab1 Webowe/src/index.ts
+++ Lab1 Webowe/src/index.ts
@@ -1,1 +1,45 @@
-console.log("test");
+
+
+class MinApp {
+    private inputs: NodeListOf<HTMLInputElement>;
+    private outputs: NodeListOf<HTMLInputElement>;
+    private arrs: number[] = [];
+    constructor() {
+        this.inputs = document.querySelectorAll(".inputs > input");
+        this.outputs = document.querySelectorAll(".outputs > input")
+
+        this.addEvents(this.inputs);
+     
+
+    }
+    private addEvents (input :NodeListOf<HTMLInputElement>) :void {
+        input.forEach((e)=>{
+            e.addEventListener("input", ()=>{
+                // this is a value from single input 
+                // do 2 things with that p
+                this.arrs.push(+e.value);
+                this.display();
+
+            })
+        })
+    }
+    private sum():number{
+      return this.arrs.reduce((a,b)=> a+b)
+    }
+    private avg():number{
+        return this.sum()/this.arrs.length
+    }
+    private sort():number[]{
+        return this.arrs.sort((a,b)=>a-b)
+    }
+    private display():void{
+        this.outputs.item(0).value = this.sum().toString(); 
+        this.outputs.item(1).value = this.avg().toString(); 
+        this.outputs.item(2).value = this.sort()[0].toString(); 
+        this.outputs.item(3).value = this.sort()[this.arrs.length-1].toString()
+    }
+  
+
+}
+
+const test = new MinApp();
